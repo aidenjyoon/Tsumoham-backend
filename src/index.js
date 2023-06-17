64003fa2641c8fe75ef38c236d68cb0a643c1beb
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-06-16 17:21:30 -0700

    Create index.js
    unfinished with twitch

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+
+const TwitchStrategy = require("passport-twitch-new").Strategy;
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(
+  `${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}}`
+);
+
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: "localhost:4000", credentials: true }));
+
+app.set("trust proxy", 1); // trust first proxy
+
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none", // since we are using new page for signing in
+      secure: true, // require httpes
+      maxAge: 1000 * 60 * 60 * 24 * 7, // one week in ms
+    },
+  })
+);
+
+// Passport middleware initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new TwitchStrategy({
+    clientId: process.env.TWITCH_CLIENT_ID,
+    clientSecret: process.env.TWITCH_CLIENT_SECRET,
+    callbackURL: "/auth/twitch/callback",
+    scope: "user_read",
+  })
+);
+
+app.get(
+  "/auth/twitch",
+  passport.authenticate("twitch", { scope: ["user:email"] })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const host = "0.0.0.0";
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
+
+app.listen(port, host, () => {
+  console.log("server started");
+});
